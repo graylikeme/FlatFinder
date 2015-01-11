b838e818f6643c32181139a593d32cc604cb64a1
Stanislav Ageev <stanislav@roomd.net>
2015-01-11 02:44:37 +0200

    Travis CI

--- travis/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-xctool -workspace FlatFinder.xcworkspace -scheme FlatFinder -configuration Debug clean -sdk iphonesimulator
-
-xctool -workspace FlatFinder.xcworkspace -scheme FlatFinder -configuration Debug
- test -sdk iphonesimulator
