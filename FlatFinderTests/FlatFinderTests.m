76bcc7d130704040eabe0dc60bb3825b54ea9cc9
Stanislav Ageev <stanislav@roomd.net>
2015-01-11 06:06:16 +0200

    Added kiwi support

--- FlatFinderTests/FlatFinderTests.m
+++ FlatFinderTests/FlatFinderTests.m
@@ -6,35 +6,16 @@
 //  Copyright (c) 2015 Stanislav Ageev. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import <XCTest/XCTest.h>
+#import "Kiwi.h"
 
-@interface FlatFinderTests : XCTestCase
+SPEC_BEGIN(MathSpec)
 
-@end
+    describe(@"Math", ^{
+        it(@"is pretty cool", ^{
+            NSUInteger a = 16;
+            NSUInteger b = 26;
+            [[theValue(a + b) should] equal:theValue(42)];
+        });
+    });
 
-@implementation FlatFinderTests
-
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
-@end
+SPEC_END
--- Podfile
+++ Podfile
@@ -1,4 +1,10 @@
+source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '7.0'
 
-pod 'Specta'
-pod 'Expecta'
+target :FlatFinder do
+  pod 'AFNetworking'
+end
+
+target :FlatFinderTests, :exclusive => true do
+  pod 'Kiwi'
+end
