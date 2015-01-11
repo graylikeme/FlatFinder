86ebcfad49c09653d170c36091576794dfd0b4f4
Stanislav Ageev <stanislav@roomd.net>
2015-01-11 02:23:50 +0200

    Fix #5

--- travis/script.sh
+++ travis/script.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 set -e
 
-xctool -workspace FlatFinder.xcworkspace -scheme FlatFinder build test
+xctool -workspace FlatFinder.xcworkspace -scheme FlatFinder -configuration Debug clean -sdk iphonesimulator
+
+xctool -workspace FlatFinder.xcworkspace -scheme FlatFinder -configuration Debug
+ test -sdk iphonesimulator
